1386999157ac83087ebc10c7467624ed1ecab7c3
Kuhu Halder <37775965+kuhuhalder@users.noreply.github.com>
2020-07-19 16:24:14 -0400

    Update ViewController.swift

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -28,7 +28,6 @@ class ViewController: UIViewController {
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     @IBAction func onTap(_ sender: Any) {
     }
